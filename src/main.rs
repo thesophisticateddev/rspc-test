8bd0b3999605bfb473ca587975e4b07b9ed18ec0
thesophisticatedidiot <salman.ahmed0071@ucp.edu.pk>
2024-04-27 09:28:59 +0200

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use axum::routing::get;
+
+
+
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    let router = rspc::Router::<()>::new()
+    .query("version", |_, _: ()| "1.0.0")
+    .build()
+    .arced();
+
+    let app = axum::Router::new()
+    .route("/", get(|| async { "Hello 'rspc'!" }))
+    .nest("/rspc", rspc_axum::endpoint(router.clone(), || ()));
+    // let addr = "[::]:9000".parse::<SocketAddr>().unwrap(); // This listens on IPv6 and IPv4
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
+    // println!("{} listening on http://{}", env!("CARGO_CRATE_NAME"), addr);
+    axum::serve(listener, app).await.unwrap();
+}
